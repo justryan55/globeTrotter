d4657cc13426ee97bcaca9457313cee6940ed232
Ryan <ryan.irani@gmail.com>
2024-08-15 20:19:21 +0100

    added travel journal card to explore page

---
+++ client/src/components/ExploreCard.tsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+const Layout = styled.div`
+  margin: 3rem;
+  padding: 1rem;
+  background-color: white;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  flex-basis: 25%;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const SubLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Header = styled.p`
+  padding-left: 30px;
+  font-size: 1.25rem;
+  font-weight: 500;
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: rgb(0, 0, 0, 0.25);
+  margin-bottom: 20px;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  height: 200px;
+  opacity: 75%;
+`;
+
+const Description = styled.p`
+  font-size: 1.3rem;
+  padding-left: 10px;
+`;
+
+export default function ExploreCard({ title, image, description }) {
+  return (
+    <Layout>
+      <Header>{title}</Header>
+      <SubLayout>
+        <Line />
+        <ImageContainer>
+          <Image src={image} />
+        </ImageContainer>
+        <Description>{description}</Description>
+      </SubLayout>
+    </Layout>
+  );
+}
+
+// Itinerary creator, google map restaurant tool, map selector
--- client/src/pages/ExplorePage.tsx
+++ client/src/pages/ExplorePage.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import NavigationBar from "../components/NavigationBar";
-import Card from "../components/Card";
+import ExploreCard from "../components/ExploreCard";
 
 const PageLayout = styled.div`
   height: 100vh;
@@ -19,25 +19,30 @@ export default function ExplorePage() {
     <PageLayout>
       <NavigationBar />
       <CardContainer>
-        <Card
+        <ExploreCard
           title="Itinerary Creator"
           image="/images/list.svg"
           description="Plan your next adventure."
         />
-        <Card
+        <ExploreCard
           title="Restaurant Finder"
           image="/images/search.svg"
           description="Identify the top-rated restaurants in your area."
         />
-        <Card
+        <ExploreCard
           title="Scratch Map"
           image="/images/earth-outline.svg"
           description="Highlight all the places you have travelled."
         />
-        <Card
+        <ExploreCard
           title="Find a Travel Buddy"
           image="/images/users.svg"
           description="Find a travel buddy for your next adventure."
+        />
+        <ExploreCard
+          title="Travel Journal"
+          image="/images/book-open.svg"
+          description="Document your travel adventures."
         />
       </CardContainer>
     </PageLayout>
