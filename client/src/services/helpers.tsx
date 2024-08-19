760cdfaeac5027e5ebbc4dff392f1783420c87f0
Ryan <ryan.irani@gmail.com>
2024-08-19 10:10:05 +0100

    defined token variable

--- client/src/services/helpers.tsx
+++ client/src/services/helpers.tsx
@@ -1,10 +1,20 @@
 export const fetchData = async (url, method, payload) => {
   try {
+    //add token to local storage. Check if token exists
+    // if token, set payload.token = to the token
+
+    const token = window.localStorage.getItem("token");
+
+    if (token) {
+      payload.token = token;
+    }
+
     const params = {
       method: method,
       headers: new Headers({
         "Content-Type": "application/json",
       }),
+      token: token,
       body: JSON.stringify(payload),
     };
 
