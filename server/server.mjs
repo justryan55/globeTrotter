0b2ba8422f19ef96f42299d1e5eb68a3a25a9a86
Ryan <ryan.irani@gmail.com>
2024-08-19 10:10:18 +0100

    changed order of 'if' statements for register

--- server/server.mjs
+++ server/server.mjs
@@ -31,22 +31,44 @@ app.post('/api/auth/register', async (req, res) => {
     const existingUser = await userModel.findOne({email})
     
     if (existingUser){
-        return res.status(500).json("An account with this email address already exists")
-    } else if (!name || !email){
-        res.status(500).json("Please complete all fields.")
+        return res.status(500).json({
+            success: false,
+            message: "An account with this email address already exists"
+        })
+    } 
+
+    if (!name){
+        return res.status(500).json({
+            success: false,
+            message: "You must enter a username"
+        })
     }
 
-    if (!existingUser && name && email && password.length>5 && password === confirmPassword){
-        const userCreated = await newUser.save()
-        res.status(200).json("User created")
-        console.log("User successfully created")
-    } else if (password !== confirmPassword){
-        res.status(500).json("Passwords do not match")
-    } else if (!password) {
-        res.status(500).json("Please enter a valid password")
+    if (!email){
+        return res.status(500).json({
+            success: false,
+            message: "You must enter an email address"
+        })
+    }
+
+    if (!password || password.length < 5){
+        return res.status(500).json({
+            success: false,
+            message: "You must enter a valid password"
+        })
+    }
+
+    if (password !== confirmPassword){
+        return res.status(500).json({
+            success: false,
+            message: "Passwords do not match"
+        })
     } else {
-        res.status(500).json("User cannot be created")
-        console.log("Error creating user")
+        const userCreated = await newUser.save()
+        return res.status(200).json({
+            success: true,
+            message: "User created"
+        })
     }
     } catch (err){
         console.log(err.message)
@@ -70,13 +92,15 @@ app.post("/api/auth/login", async(req, res) => {
 
     const secretKey = process.env.SECRET_KEY
 
+
     const payload = {
         name: user.name,
         email: user.email,
-        password: user.password
+        password: user.password,
     }
 
     const token = jwt.sign(payload, secretKey, { expiresIn: "5d" })
+
 
     res.status(200).json({
         message: "User is logged in",
