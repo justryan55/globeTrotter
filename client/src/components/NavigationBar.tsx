f55ee52c7232e279347e6c393b8749d04d04a2b2
Ryan <ryan.irani@gmail.com>
2024-08-19 10:10:55 +0100

    added logout button

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -81,10 +81,18 @@ const MenuText = styled.p`
 export default function NavigationBar() {
   const [activeItem, setActiveItem] = useState();
   const navigate = useNavigate();
+  const token = window.localStorage.getItem("token");
 
   const handleClick = (item) => {
     setActiveItem(item);
     navigate("/" + item.toLowerCase());
+  };
+
+  const handleLogout = () => {
+    if (token) {
+      localStorage.removeItem("token");
+      navigate("/");
+    }
   };
 
   return (
@@ -201,6 +209,25 @@ export default function NavigationBar() {
           <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
         </MenuImg>
         <MenuText>Settings</MenuText>
+      </MenuLayout>
+      <MenuLayout onClick={handleLogout}>
+        <MenuImg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="nav-icon"
+        >
+          <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+          <polyline points="16 17 21 12 16 7"></polyline>
+          <line x1="21" y1="12" x2="9" y2="12"></line>
+        </MenuImg>
+        <MenuText>Log Out</MenuText>
       </MenuLayout>
     </Layout>
   );
