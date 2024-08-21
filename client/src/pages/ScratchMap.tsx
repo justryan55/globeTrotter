bfc408ee933f4917e7ed0a7f9e40a7097a962b2c
Ryan <ryan.irani@gmail.com>
2024-08-21 16:07:23 +0100

    added country api

--- client/src/pages/ScratchMap.tsx
+++ client/src/pages/ScratchMap.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import NavigationBar from "../components/NavigationBar";
+import { useEffect, useState } from "react";
 
 const PageLayout = styled.div`
   height: 100vh;
@@ -8,10 +9,59 @@ const PageLayout = styled.div`
   background-color: #f8f8ff;
 `;
 
+const CardLayout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`;
+
+const CountryCard = styled.div`
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 export default function ScratchMap() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    try {
+      const getData = async () => {
+        const res = await fetch("https://restcountries.com/v3.1/all");
+        if (!res.ok) {
+          throw new Error(`Response status: ${res.status}`);
+        }
+
+        const data = await res.json();
+        setCountries(data);
+      };
+
+      getData();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  const handleClick = (country) => {
+    console.log(country);
+  };
+
   return (
     <PageLayout>
       <NavigationBar />
+      <CardLayout>
+        {countries.map((country) => {
+          const countryName = country.name.common;
+          return (
+            <CountryCard
+              onClick={() => {
+                handleClick(countryName);
+              }}
+            >
+              {countryName}
+            </CountryCard>
+          );
+        })}
+      </CardLayout>
     </PageLayout>
   );
 }
