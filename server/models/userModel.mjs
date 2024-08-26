46f3f02b2ce6a6a9045bf88e7be01b7505852606
Ryan <ryan.irani@gmail.com>
2024-08-26 16:34:22 +0100

    added countries_visited property

--- server/models/userModel.mjs
+++ server/models/userModel.mjs
@@ -7,6 +7,7 @@ const userSchema = new Schema(
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    countries_visited: [{ type: String }],
   },
   { timestamps: true }
 );
@@ -14,9 +15,6 @@ const userSchema = new Schema(
 const userModel = mongoose.model("User", userSchema);
 
 export default userModel;
-
-// Todo - 05 - Create a new property on the user model called something like countries_visited which is an array of strings, these strings will eventually
-//             map to the country model code
 
 // Todo - 05 - Create a new property on the user model called something like countries_visited which is an array of strings, these strings will eventually
 //             map to the country model code
