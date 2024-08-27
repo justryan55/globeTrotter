06cc9a9a4bf74995259cf26b1f78594a76dc9f10
Ryan <ryan.irani@gmail.com>
2024-08-27 11:04:04 +0100

    added google map api

---
+++ client/src/components/GoogleMapComponent.tsx
@@ -0,0 +1,25 @@
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+export default function GoogleMapComponent() {
+  const googleAPIKey = import.meta.env.VITE_GOOGLE_API_KEY;
+
+  const mapContainerStyle = {
+    height: "100vh",
+    width: "100%",
+  };
+
+  const center = {
+    lat: -33.8688,
+    lng: 151.2093,
+  };
+
+  return (
+    <LoadScript googleMapsApiKey={googleAPIKey}>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        center={center}
+        zoom={10}
+      ></GoogleMap>
+    </LoadScript>
+  );
+}
