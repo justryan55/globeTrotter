c0d52bccdff0654a9f426ee4d5cb197352ff21b9
Ryan <ryan.irani@gmail.com>
2024-08-29 15:49:26 +0100

    added post model

---
+++ server/models/postModel.mjs
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    postedBy: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    totalLikes: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const postModel = mongoose.model("Post", postSchema);
+
+export default postModel;
