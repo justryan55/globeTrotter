7feaeab6feac1e7bb1ddc2aa8b7c82d35689dd65
Ryan <ryan.irani@gmail.com>
2024-09-03 08:59:06 +0100

    added isLoading and isAuthenticated state

--- client/src/services/AuthContext.tsx
+++ client/src/services/AuthContext.tsx
@@ -4,6 +4,9 @@ import { fetchData } from "./helpers";
 export const UserContext = createContext(null);
 
 export default function AuthContext({ children }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   const [user, setUser] = useState({
     userId: "",
     firstName: "",
@@ -12,28 +15,37 @@ export default function AuthContext({ children }) {
     countriesVisited: "",
   });
 
+  const getUserData = async () => {
+    try {
+      const backendURL = import.meta.env.VITE_BACKEND_URL;
+
+      const res = await fetchData(`${backendURL}/api/auth/getUser`, "GET");
+      const data = await res.json();
+
+      const { userId, firstName, lastName, email, countriesVisited } =
+        data.payload;
+
+      setUser({ userId, firstName, lastName, email, countriesVisited });
+      setIsLoading(false);
+      setIsAuthenticated(true);
+    } catch (err) {
+      console.log("Error fetching user data:", err);
+    }
+  };
+
   useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const backendURL = import.meta.env.VITE_BACKEND_URL;
-
-        const res = await fetchData(`${backendURL}/api/auth/getUser`, "GET");
-        const data = await res.json();
-
-        const { userId, firstName, lastName, email, countriesVisited } =
-          data.payload;
-
-        setUser({ userId, firstName, lastName, email, countriesVisited });
-      } catch (err) {
-        console.log("Error fetching user data:", err);
-      }
-    };
     getUserData();
-  }, []);
+  }, [isAuthenticated]);
 
   return (
-    <UserContext.Provider value={[user, setUser]}>
-      {children}
-    </UserContext.Provider>
+    <>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <UserContext.Provider value={[user, setUser]}>
+          {children}
+        </UserContext.Provider>
+      )}
+    </>
   );
 }
