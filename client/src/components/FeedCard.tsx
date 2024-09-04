099a00d972a61fc75356043ac07f2349d92774d5
Ryan <ryan.irani@gmail.com>
2024-09-04 16:09:59 +0100

    formatted timestamp

--- client/src/components/FeedCard.tsx
+++ client/src/components/FeedCard.tsx
@@ -49,6 +49,29 @@ export default function FeedCard() {
     setPosts(data.details);
   };
 
+  const formatTimestamp = (timestamp) => {
+    const current = new Date();
+    const provided = new Date(timestamp);
+    const timeDifference = current - provided;
+    const minutes = Math.floor(timeDifference / (1000 * 60));
+    const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    if (minutes === 0) {
+      return "Just now";
+    }
+
+    if (minutes < 60) {
+      return `${minutes} minute${minutes === 1 ? "" : "s"} ago`;
+    }
+
+    if (hours < 24) {
+      return `${hours} hour${hours === 1 ? "" : "s"} ago`;
+    } else {
+      return `${days} day${days === 1 ? "" : "s"} ago`;
+    }
+  };
+
   useEffect(() => {
     fetchPosts();
   }, [userid]);
@@ -60,7 +83,7 @@ export default function FeedCard() {
       <CreateNewPost onPostCreated={fetchPosts} />
       <SubLayout>
         {posts.length === 0 ? (
-          <p>Loading...</p>
+          <p>There are no posts in your feed.</p>
         ) : (
           posts &&
           posts.map((post) => {
@@ -69,7 +92,7 @@ export default function FeedCard() {
                 postId={post.postId}
                 profileImage="/images/avatar.png"
                 name={post.postedBy}
-                timestamp={post.createdAt}
+                timestamp={formatTimestamp(post.createdAt)}
                 content={post.content}
               />
             );
