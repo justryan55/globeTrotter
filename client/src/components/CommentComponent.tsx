1a88b9394f64c45ae1626b0a8b27b1fee582ca6b
Ryan <ryan.irani@gmail.com>
2024-09-04 16:09:49 +0100

    formatted timestamp and added comment action buttons

--- client/src/components/CommentComponent.tsx
+++ client/src/components/CommentComponent.tsx
@@ -15,7 +15,7 @@ const ExistingComments = styled.div`
   border-radius: 8px;
   outline: none;
   resize: none;
-  background-color: #f9f9f9;
+  background-color: white;
 `;
 
 const ExistingComment = styled.div`
@@ -25,10 +25,10 @@ const ExistingComment = styled.div`
   align-items: center;
   box-sizing: border-box;
   width: 100%;
+  background-color: #f9f9f9;
 
   padding: 10px;
   min-height: 50px;
-  margin-bottom: 10px;
   font-size: 16px;
   border: 1px solid rgb(204, 204, 204, 0.5);
   border-radius: 8px;
@@ -66,6 +66,18 @@ const NewCommentBox = styled.textarea`
   color: #333;
 `;
 
+const CommentActions = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: white;
+  font-size: 14px;
+  padding-left: 10px;
+  gap: 1rem;
+  /* margin-bottom: 10px; */
+`;
+
+const Action = styled.p``;
+
 export default function CommentComponent({ postId }) {
   const backendURL = import.meta.env.VITE_BACKEND_URL;
   const [user] = useContext(UserContext);
@@ -78,6 +90,29 @@ export default function CommentComponent({ postId }) {
     postedBy: "",
     comment: "",
   });
+
+  const formatTimestamp = (timestamp) => {
+    const current = new Date();
+    const provided = new Date(timestamp);
+    const timeDifference = current - provided;
+    const minutes = Math.floor(timeDifference / (1000 * 60));
+    const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    if (minutes === 0) {
+      return "Just now";
+    }
+
+    if (minutes < 60) {
+      return `${minutes} minute${minutes === 1 ? "" : "s"} ago`;
+    }
+
+    if (hours < 24) {
+      return `${hours} hour${hours === 1 ? "" : "s"} ago`;
+    } else {
+      return `${days} day${days === 1 ? "" : "s"} ago`;
+    }
+  };
 
   const handleSubmit = (e) => {
     if (e.key === "Enter") {
@@ -128,13 +163,20 @@ export default function CommentComponent({ postId }) {
             const message = comment.comment;
 
             return (
-              <ExistingComment key={comment.id}>
-                <div>
-                  <Sender>{postedBy}</Sender>
-                  <Comment>{message}</Comment>
-                </div>
-                <Timestamp>{timestamp}</Timestamp>
-              </ExistingComment>
+              <>
+                <ExistingComment key={comment.id}>
+                  <div>
+                    <Sender>{postedBy}</Sender>
+                    <Comment>{message}</Comment>
+                  </div>
+                </ExistingComment>
+                <CommentActions>
+                  <Timestamp>{formatTimestamp(timestamp)}</Timestamp>
+
+                  <Action>Like</Action>
+                  <Action>Reply</Action>
+                </CommentActions>
+              </>
             );
           })}
         </ExistingComments>
