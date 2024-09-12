d5633e7e3ae58dddbf6774fcab33d1933be908cd
Ryan <ryan.irani@gmail.com>
2024-09-12 15:38:27 +0100

    added error handling

--- server/config/helpers.mjs
+++ server/config/helpers.mjs
@@ -2,29 +2,43 @@ import jwt from "jsonwebtoken";
 
 export const checkToken = async (authHeader) => {
   if (authHeader) {
-    const token = authHeader.split(" ")[1];
-    const secretKey = process.env.SECRET_KEY;
-    const decodedToken = jwt.verify(token, secretKey);
+    try {
+      const token = authHeader.split(" ")[1];
+      const secretKey = process.env.SECRET_KEY;
+      const decodedToken = jwt.verify(token, secretKey);
 
-    if (decodedToken) {
-      const userId = decodedToken.userId;
-      const firstName = decodedToken.firstName;
-      const lastName = decodedToken.lastName;
-      const email = decodedToken.email;
-      const countriesVisited = decodedToken.countries_visited;
+      if (decodedToken) {
+        const {
+          userId,
+          firstName,
+          lastName,
+          email,
+          countries_visited: countriesVisited,
+        } = decodedToken;
 
-      return {
-        userId,
-        firstName,
-        lastName,
-        email,
-        countriesVisited,
-      };
-    } else {
+        return {
+          userId,
+          firstName,
+          lastName,
+          email,
+          countriesVisited,
+        };
+      } else {
+        return {
+          success: false,
+          message: "Not authorised",
+        };
+      }
+    } catch (err) {
       return {
         success: false,
-        message: "Not authorised",
+        message: "Invalid or expired token",
       };
     }
+  } else {
+    return {
+      success: false,
+      message: "No token provided",
+    };
   }
 };
