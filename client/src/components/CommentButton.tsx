303ad4bd6a68e79f1a2ca970c805da47b08319e7
Ryan <ryan.irani@gmail.com>
2024-09-16 09:40:48 +0100

    updated svg properties

--- client/src/components/CommentButton.tsx
+++ client/src/components/CommentButton.tsx
@@ -32,9 +32,9 @@ export default function CommentButton(postId: string) {
         viewBox="0 0 24 24"
         fill="none"
         stroke="black"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
       </ActionButton>
