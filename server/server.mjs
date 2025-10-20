15f7cc9bec833481c2eacfa75d601d6352961680
Ryan Irani <ryan.irani@gmail.com>
2025-10-20 21:42:23 +0200

    chore: remove trailing /

--- server/server.mjs
+++ server/server.mjs
@@ -15,7 +15,7 @@ const port = 3000;
 app.use(
   cors({
     origin:
-      "https://travel-project-a7pfbrmkb-ryans-projects-20a8834f.vercel.app/",
+      "https://travel-project-a7pfbrmkb-ryans-projects-20a8834f.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
